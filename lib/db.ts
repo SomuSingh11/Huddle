976566375ebc410415536faed3511ddab6d768f6
SOMU SINGH <somusingh0110@gmail.com>
2024-09-04 04:53:49 +0530

    Prisma and Neondb Setup

---
+++ lib/db.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+// This ensures that hot reload in Nextjs doesnt initialise too many Prisma Clients
