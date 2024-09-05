135e9a39c12639aa370ff2ae8bfed7f1c3f5f425
SOMU SINGH <somusingh0110@gmail.com>
2024-09-05 15:21:47 +0530

    Setup Upload Things ( "Implement image upload with preview and reset button for server configuration." )

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,29 @@
+import { auth } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+const handleAuth = () => {
+  const { userId } = auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+  return { userId: userId };
+};
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(
+      () => handleAuth() // This code runs on your server before upload, wont upload if handleAuth() throws error
+    )
+    .onUploadComplete(() => {}),
+
+  // For uploading attachments in messages
+  messageFile: f(["image", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
