e69abc4bd1b5c5c9b12048ddf2d2ef8dcf837bc8
SOMU SINGH <somusingh0110@gmail.com>
2024-09-07 16:35:48 +0530

    Add ServerIdLayout

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,47 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+
+const ServerIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { serverId: string };
+}) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
+
+  // Fetch the server from the database using the server ID and check if the current profile is a member
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id, // Ensure that the profile is a member of the server
+        },
+      },
+    },
+  });
+
+  // If the server is not found or the profile is not a member, redirect to the home page
+  if (!server) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full ">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        Server Sidebar
+      </div>
+      <main className="h-full md:ml-60">{children}</main>
+    </div>
+  );
+};
+
+export default ServerIdLayout;
