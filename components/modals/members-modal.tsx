a451bca58b63dc70b6e1dd6c82ae506afdaa802c
SOMU SINGH <somusingh0110@gmail.com>
2024-09-09 12:21:45 +0530

    Implemented a members modal for server management, allowing users to view member profiles, assign roles (Guest, Moderator), and kick members. Added a loading spinner for actions in progress and used a scrollable area to handle large member lists. Integrated dropdown menus for role assignment and member management actions.

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -1,6 +1,18 @@
 "use client";
 
 import {
+  Check,
+  Gavel,
+  Loader2,
+  MoreVertical,
+  Shield,
+  ShieldAlert,
+  ShieldCheck,
+  ShieldQuestion,
+} from "lucide-react";
+import { useState } from "react";
+
+import {
   Dialog,
   DialogContent,
   DialogDescription,
@@ -8,12 +20,32 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuTrigger,
+  DropdownMenuSubTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { useModal } from "@/hooks/use-modal-store";
 import { ServerWithMembersWithProfiles } from "@/types";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { UserAvatar } from "@/components/user-avatar";
+
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
+};
 
 export const MembersModal = () => {
   const { onOpen, isOpen, onClose, type, data } = useModal(); //Whenever invitation Modal is called in "server-header.tsx", server data is passed and hence is extracted
+  const [loadingId, setLoadingId] = useState(""); // State to track the ID of a member that is being processed (e.g., kicked or role change)
 
   const isModalOpen = isOpen && type === "members"; // TypeScript type assertion: It tells TypeScript to treat data as an object that contains a server property of type ServerWithMembersWithProfiles.
   const { server } = data as { server: ServerWithMembersWithProfiles };
@@ -21,7 +53,7 @@ export const MembersModal = () => {
   return (
     // The onOpenChange prop is a callback function that gets triggered whenever there is a change in the open state of the dialog (i.e., when the modal is opened or closed).
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+      <DialogContent className="bg-white text-black overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
             Manage Members
@@ -30,7 +62,72 @@ export const MembersModal = () => {
             {server?.members?.length} Members
           </DialogDescription>
         </DialogHeader>
-        <div className="p-6">Hello Members</div>
+
+        <ScrollArea className="mt-8 max-h-[420px] pr-6">
+          {/* Loop over members array and display each member's info */}
+          {server?.members?.map((member) => (
+            <div key={member.id} className="flex items-center gap-x-2 mb-6">
+              <UserAvatar src={member.profile.imageUrl} />
+              <div className="flex flex-col gap-y-1">
+                <div className="text-xs font-semibold flex items-center ">
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+                <p className="text-xs text-zinc-500">{member.profile.email}</p>
+              </div>
+
+              {/* Ensure that the logged-in user is not owner of the Server and can't change their own role and no action is being performed on this member right now*/}
+              {server.profileId !== member.profileId &&
+                loadingId !== member.id && (
+                  <div className="ml-auto">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <MoreVertical className="h-4 w-4 text-zinc-500" />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent side="left">
+                        <DropdownMenuSub>
+                          <DropdownMenuSubTrigger className="flex items-center">
+                            <ShieldQuestion className="h-4 w-4 mr-2" />
+                            <span>Role</span>
+                          </DropdownMenuSubTrigger>
+                          <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                              <DropdownMenuItem>
+                                <Shield className="h-4 w-4 mr-2 " />
+                                Guest
+                                {member.role === "GUEST" && (
+                                  <Check className="h-4 w-4 ml-auto" />
+                                )}
+                              </DropdownMenuItem>
+                              <DropdownMenuItem>
+                                <ShieldCheck className="h-4 w-4 mr-2 " />
+                                Moderator
+                                {member.role === "MODERATOR" && (
+                                  <Check className="h-4 w-4 ml-auto" />
+                                )}
+                              </DropdownMenuItem>
+                            </DropdownMenuSubContent>
+                          </DropdownMenuPortal>
+                        </DropdownMenuSub>
+
+                        <DropdownMenuSeparator />
+
+                        <DropdownMenuItem>
+                          <Gavel className="h-4 w-4 mr-2" />
+                          Kick
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                )}
+
+              {/* Show a loader when an action is being performed on this member */}
+              {loadingId === member.id && (
+                <Loader2 className="animate-spin text-zinc-500 ml-auto w-4 h-4 " />
+              )}
+            </div>
+          ))}
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   );
