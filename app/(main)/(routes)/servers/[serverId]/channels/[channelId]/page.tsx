43768880ca44a8dfe295591747f181ce9997d735
SOMU SINGH <somusingh0110@gmail.com>
2024-09-13 18:53:25 +0530

    Add ChannelIdPage component to render chat channel with member validation.

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,48 @@
-const ChannelIdPage = () => {
-  return <div>Channel Id Page!</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { auth } from "@clerk/nextjs/server";
+
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { ChatHeader } from "@/components/chat/chat-header";
+
+interface ChannelIdPage {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPage) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
+
+  // Query the database to fetch the channel by its ID
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params?.channelId,
+    },
+  });
+
+  // Query the database to check if the user is a member of the server
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params?.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  // If the channel does not exist or the user is not a member, redirect to the homepage
+  if (!channel || !member) {
+    redirect("/");
+  }
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
