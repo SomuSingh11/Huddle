892f8ee8d87f2272b1788d62d2c59a885c0422c2
SOMU SINGH <somusingh0110@gmail.com>
2024-09-15 12:24:43 +0530

    recreating another current-profile page that works for page routing

---
+++ lib/current-profile-pages.ts
@@ -0,0 +1,24 @@
+import { getAuth } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+import { NextApiRequest } from "next";
+
+export const currentProfile = async (req: NextApiRequest) => {
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    return null;
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: userId,
+    },
+  });
+
+  return profile;
+};
+
+// The currentProfile function is a reusable utility that performs a common task:
+// fetching the current user's profile from the database based on their authentication status
+
+// -----> current-profile works only for app routing and this we need "current-profile-pages" for pages routing
