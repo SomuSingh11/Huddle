b4e6c9feeb625d1611a5b874fbd55d32eaf06926
SOMU SINGH <somusingh0110@gmail.com>
2024-09-17 04:03:41 +0530

    Setup LiveKit for integrating video conferencing

---
+++ components/media-room.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import "@livekit/components-styles";
+import { Channel } from "@prisma/client";
+import { useUser } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+
+interface MediaRoomsProps {
+  chatID: string;
+  video: boolean;
+  audio: boolean;
+}
+
+export const MediaRoom = ({ chatID, video, audio }: MediaRoomsProps) => {
+  const { user } = useUser();
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    if (!user?.firstName || !user?.lastName) return;
+
+    const name = `${user.firstName} ${user.lastName}`;
+
+    async () => {
+      try {
+        const resp = await fetch(
+          `/api/livekit?room=${chatID}&username=${name}`
+        );
+        const data = await resp.json();
+        setToken(data.token);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+  }, [user?.firstName, user?.lastName, chatID]);
+
+  if (token == "") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <LiveKitRoom
+      video={true}
+      audio={true}
+      token={token}
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      // Use the default LiveKit theme for nice styles.
+      data-lk-theme="default"
+      connect={true}
+    >
+      {/* Your custom component with basic video conferencing functionality. */}
+      <VideoConference />
+    </LiveKitRoom>
+  );
+};
